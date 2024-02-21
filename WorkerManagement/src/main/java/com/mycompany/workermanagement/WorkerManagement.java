d58e327f6239ed0d190db3b65ef503ec0ed2aae1
Quang <Sewrffde@gmail.com>
2024-02-21 08:45:34 +0700

    u

--- WorkerManagement/src/main/java/com/mycompany/workermanagement/WorkerManagement.java
+++ WorkerManagement/src/main/java/com/mycompany/workermanagement/WorkerManagement.java
@@ -1,16 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-package com.mycompany.workermanagement;
-
-/**
- *
- * @author quoch
- */
-public class WorkerManagement {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public static void printListPerson(ArrayList<Person> lp, double money) {
+        System.out.printf("%-20s%-20s%-20s\n", "Name", "Address", "Money");
+        for (Person person : lp) {
+            if (person.getMoney() >= money) {
+                System.out.printf("%-20s%-20s%-20.1f\n", person.getName(),
+                        person.getAddress(), person.getMoney());
+            }
+        }
+        Collections.sort(lp);
+        System.out.println("Max: " + lp.get(0).getName());
+        System.out.println("Min: " + lp.get(lp.size() - 1).getName());
     }
-}
